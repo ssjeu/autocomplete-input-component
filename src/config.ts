ee7863720e9a335178cac6455b0d281ac007349d
ssjeu <sje1015s@gmail.com>
2022-11-10 09:33:48 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   EN_URL:
-    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/aecf71f2-ef10-4c64-ac79-300586539076/generated.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221027%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221027T142025Z&X-Amz-Expires=86400&X-Amz-Signature=d05bf21291d31e5767a7427561449faa130247114fd963d65bd7f1eaf0e9040c&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22generated.json%22&x-id=GetObject",
+    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/aecf71f2-ef10-4c64-ac79-300586539076/generated.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221107%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221107T104104Z&X-Amz-Expires=86400&X-Amz-Signature=530b1bed8d97e568d8c1ed78e09c29d76a9bae0f2530dba2cbf23eb08f8d74db&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22generated.json%22&x-id=GetObject",
   KO_URL:
     "https://adearth-bucket.s3.ap-northeast-2.amazonaws.com/MOCK_DATA.json",
 };
